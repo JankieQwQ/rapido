5efae5a5a686fc5470a1da5da37425e5021129fa
Jankie <qwq@awaish.link>
2024-01-25 19:36:45 +0800

    Update safe mode

--- src/rapido.cpp
+++ src/rapido.cpp
@@ -2,11 +2,27 @@
 
 void rprint(const char* str,char endl='\n',int safe=0){
     #ifdef __GNUC__
-        // 在 GUNC 环境下，关闭流同步，不使用endl换行的std::cout是最快的。
         if(!safe){
             std::ios::sync_with_stdio(false);
+            std::cout << str << endl;
+        }else{
+            __asm__(
+                "push    rbp\n"
+                "mov     rbp, rsp\n"
+                "sub     rsp, 16\n"
+                "mov     QWORD PTR [rbp-8], rdi\n"
+                "mov     rdx, QWORD PTR stdout[rip]\n"
+                "mov     rax, QWORD PTR [rbp-8]\n"
+                "mov     rsi, rdx\n"
+                "mov     rdi, rax\n"
+                "call    fputs\n"
+                "mov     edi, 10\n"
+                "call    putchar\n"
+                "nop\n"
+                "leave\n"
+                "ret\n"
+        );
         }
-        std::cout << str << endl;
     #else 
         __asm__(
         "push    rbp\n"
